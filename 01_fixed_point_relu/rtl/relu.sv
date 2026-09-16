e77f32b092f89d803dc8f20393ca22f04b974f0c
b0niiy <limekbk@ewha.ac.kr>
2026-09-16 19:45:22 +0900

    Fix comment formatting in relu.sv

--- 01_fixed_point_relu/rtl/relu.sv
+++ 01_fixed_point_relu/rtl/relu.sv
@@ -6,8 +6,7 @@ module relu #(
 );
 
     always_comb begin
-        // signed 타입이므로 0보다 작은지(음수) 직접 비교하는 것이 가장 안전합니다.
-        if (in_data < 0) begin
+              if (in_data < 0) begin
             out_data = {WIDTH{1'b0}}; // 음수면 0으로 채움
         end else begin
             out_data = in_data;       // 양수면 그대로 출력
